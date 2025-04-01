30012a9e90c261270f3b38d40d8abea159a0a859
yichi170 <yichi170@gmail.com>
2025-03-31 23:01:00 -0500

    implement loop info

--- include/UnitLoopInfo.h
+++ include/UnitLoopInfo.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_UNIT_LOOP_INFO_H
 #define INCLUDE_UNIT_LOOP_INFO_H
 #include "llvm/IR/PassManager.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 
@@ -9,9 +10,102 @@ namespace ece479k
 /// An object holding information about the (natural) loops in an LLVM
 /// function. At minimum this will need to identify the loops, may hold
 /// additional information you find useful for your LICM pass
+class SingleLoop {
+  llvm::BasicBlock *header;
+  llvm::BasicBlock *latch;
+  llvm::BasicBlock *preheader;
+  llvm::SmallVector<BasicBlock *> body;
+
+public:
+  SingleLoop(llvm::BasicBlock *header,
+             llvm::BasicBlock *latch,
+             const llvm::SmallVector<BasicBlock *>& body) :
+    header(header), latch(latch), body(body) {}
+
+  llvm::SmallVector<BasicBlock *>& getBody() {
+    return body;
+  }
+
+  llvm::BasicBlock *getPreheader() {
+    if (preheader) return preheader;
+    if (!header) return nullptr;
+    llvm::Function *F = header->getParent();
+    if (!F) return nullptr;
+
+    // Check if the header already has a preheader (single predecessor)
+    if (!header->hasNPredecessorsOrMore(2)) {
+      llvm::dbgs() << "Use existed preheader\n";
+      return *pred_begin(header);
+    }
+
+    llvm::LLVMContext &Ctx = header->getContext();
+    preheader = llvm::BasicBlock::Create(Ctx, "preheader", F, header);
+
+    // Redirect all predecessors of the loop header to the new preheader
+    std::vector<llvm::BasicBlock *> preds(pred_begin(header), pred_end(header));
+
+    for (llvm::BasicBlock *pred : preds) {
+      llvm::Instruction *TI = pred->getTerminator();
+      if (!TI) continue;
+
+      // Replace conditional branches that target the loop header
+      for (unsigned i = 0, n = TI->getNumSuccessors(); i < n; ++i) {
+        if (TI->getSuccessor(i) == header) {
+          TI->setSuccessor(i, preheader);
+        }
+      }
+    }
+
+    llvm::BranchInst::Create(header, preheader);
+
+    return preheader;
+  }
+
+
+  bool containInst(llvm::Instruction* I) {
+    for (auto *BB: body) {
+      if (I->isUsedInBasicBlock(BB))
+        return true;
+    }
+    return false;
+  }
+
+  bool checkInvariant(llvm::Instruction& I) {
+    if (isa<llvm::PHINode>(&I)) {
+      dbgs() << "Skipping PHI node\n";
+      return false; // Return false early, as PHI nodes cannot be LICM
+    }
+
+    bool isInv = true;
+    dbgs() << "operand: ";
+    for (auto& op : I.operands()) {
+      dbgs() << " " << op.get()->getName();
+
+      if (auto* opInst = dyn_cast<llvm::Instruction>(op.get())) {
+        if (containInst(opInst)) {
+          isInv = false;
+          break;
+        }
+      }
+    }
+    dbgs() << "\n";
+    return isInv;
+  }
+};
+
 class UnitLoopInfo
 {
     // Define this class to provide the information you need in LICM
+public:
+  void add(llvm::BasicBlock *header,
+           llvm::BasicBlock *latch,
+           const llvm::SmallVector<BasicBlock *>& body);
+
+  llvm::SmallVector<SingleLoop *>& getLoops() {
+    return loops;
+  }
+private:
+  llvm::SmallVector<SingleLoop *> loops;
 };
 
 /// Loop Identification Analysis Pass. Produces a UnitLoopInfo object which
--- lib/UnitLoopInfo.cpp
+++ lib/UnitLoopInfo.cpp
@@ -4,6 +4,7 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include "UnitLoopInfo.h"
+#include <queue>
 
 using namespace llvm;
 using namespace ece479k;
@@ -16,11 +17,70 @@ UnitLoopInfo UnitLoopAnalysis::run(Function &F, FunctionAnalysisManager &FAM) {
   // Acquires the Dominator Tree constructed by LLVM for this function. You may
   // find this useful in identifying the natural loops
   DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
-  
+
   UnitLoopInfo Loops;
   // Fill in appropriate information
+  std::set<BasicBlock *> visited;
+  std::vector<std::pair<BasicBlock *, BasicBlock *>> backEdges;
+  for (auto &BB: F) {
+    visited.insert(&BB);
+    for (auto *Suc: successors(&BB)) {
+      // is visited!
+      if (visited.find(Suc) != visited.end()) {
+        dbgs() << "Found a back edge: " << BB.getName()
+               << " to " << Suc->getName() << "\n";
+        backEdges.emplace_back(&BB, Suc);
+      }
+    }
+  }
+
+  for (auto [latch, header]: backEdges) {
+    visited.clear();
+    SmallVector<BasicBlock *> loopBB;
+    bool is_loop = true;
+    std::queue<BasicBlock *> q;
+    q.push(latch);
+    loopBB.emplace_back(latch);
+    visited.insert(latch);
+
+    while (q.size()) {
+      BasicBlock *child = q.front();
+      q.pop();
+
+      if (child == header)
+        break;
+
+      for (auto *parent: predecessors(child)) {
+        if (visited.find(parent) == visited.end() &&
+            DT.dominates(header, parent)) {
+          q.emplace(parent);
+          loopBB.emplace_back(parent);
+          visited.insert(parent);
+        } else {
+          is_loop = false;
+          break;
+        }
+      }
+
+      if (!is_loop)
+        break;
+    }
+
+    if (is_loop) {
+      dbgs() << "Found a loop with a header: " << header->getName() << "\n";
+      std::reverse(loopBB.begin(), loopBB.end());
+      Loops.add(header, latch, loopBB);
+    }
+  }
 
   return Loops;
 }
 
 AnalysisKey UnitLoopAnalysis::Key;
+
+void UnitLoopInfo::add(llvm::BasicBlock *header,
+                       llvm::BasicBlock *latch,
+                       const llvm::SmallVector<BasicBlock *>& body)
+{
+  this->loops.push_back(new SingleLoop(header, latch, body));
+}
